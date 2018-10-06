8c1ee9cf368cc46f9ba3adb0405d474ca33fce70
Likit Preeyanon <likit.pre@mahidol.edu>
2018-10-06 18:12:41 +0700

    updated the models according to new requirements

--- app/models.py
+++ app/models.py
@@ -4,17 +4,20 @@ from .main import db
 
 class Participant(db.Model):
     __tablename__ = 'participants'
-    id = Column('id', Integer, primary_key=True, autoincrement=False)
-    title = Column('title', String())
-    firstname = Column('firstname', String())
-    lastname = Column('lastname', String())
-    email = Column('email', String())
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    old_id = Column('old_id', Integer)
+    title = Column('title', String(), nullable=True)
+    firstname = Column('firstname', String(), nullable=False)
+    lastname = Column('lastname', String(), nullable=False)
+    email = Column('email', String(), nullable=False)
     faculty = Column('faculty', String(), nullable=True)
-    affiliation = Column('affil', String())
-    mobile = Column('mobile', String())
-    delivery_address = Column('delivery_address', String())
-    position_type = Column('position_type', String())
+    affiliation = Column('affil', String(), nullable=True)
+    mobile = Column('mobile', String(), nullable=True)
+    delivery_address = Column('delivery_address', String(), nullable=True)
+    position_type = Column('position_type', String(), nullable=True)
     registers = relationship('Registration', backref="participant")
+    role_id = Column('role_id', Integer(), ForeignKey('roles.id'))
+    role = relationship('Role', backref='participants')
 
 
 class Registration(db.Model):
@@ -23,4 +26,13 @@ class Registration(db.Model):
     registered_at = Column('registered_at', DateTime())
     participant_id = Column('participant_id', ForeignKey('participants.id'))
     checked_at = Column('checked_at', DateTime())
+    payment_required = Column('payment_required', Boolean(), default=False)
+    pay_status = Column('pay_status', Boolean(), default=False)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    desc = Column('desc', String())
+
 
